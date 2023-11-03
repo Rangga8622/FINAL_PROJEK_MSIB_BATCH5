81cb9dbd562867f757f1a67e5cc1c6cc013d22ac
Rangga8622 <ranggaekkle20020806@gmail.com>
2023-11-03 22:27:53 +0700

    update laravel

---
+++ resources/views/frontend/about.blade.php
@@ -0,0 +1,106 @@
+
+<div class="page-section" id="about">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 py-3 wow fadeInUp">
+                    <span class="subhead">About us</span>
+                    <h2 class="title-section">The number #1 SEO Service Company</h2>
+                    <div class="divider"></div>
+
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+                        ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                    <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.</p>
+                    <a href="about.html" class="btn btn-primary mt-3">Read More</a>
+                </div>
+                <div class="col-lg-6 py-3 wow fadeInRight">
+                    <div class="img-fluid py-3 text-center">
+                        <img src="../assets/img/about_frame.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div> <!-- .container -->
+</div> <!-- .page-section -->
+<div class="page-section bg-light">
+        <div class="container">
+            <div class="text-center wow fadeInUp">
+                <div class="subhead">Our services</div>
+                <h2 class="title-section">How SEO Team Can Help</h2>
+                <div class="divider mx-auto"></div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
+                    <div class="features">
+                        <div class="header mb-3">
+                            <span class="mai-business"></span>
+                        </div>
+                        <h5>OnSite SEO</h5>
+                        <p>We analyse your website's structure, internal architecture & other key</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
+                    <div class="features">
+                        <div class="header mb-3">
+                            <span class="mai-business"></span>
+                        </div>
+                        <h5>OnSite SEO</h5>
+                        <p>We analyse your website's structure, internal architecture & other key</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
+                    <div class="features">
+                        <div class="header mb-3">
+                            <span class="mai-business"></span>
+                        </div>
+                        <h5>OnSite SEO</h5>
+                        <p>We analyse your website's structure, internal architecture & other key</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
+                    <div class="features">
+                        <div class="header mb-3">
+                            <span class="mai-business"></span>
+                        </div>
+                        <h5>OnSite SEO</h5>
+                        <p>We analyse your website's structure, internal architecture & other key</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
+                    <div class="features">
+                        <div class="header mb-3">
+                            <span class="mai-business"></span>
+                        </div>
+                        <h5>OnSite SEO</h5>
+                        <p>We analyse your website's structure, internal architecture & other key</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
+                    <div class="features">
+                        <div class="header mb-3">
+                            <span class="mai-business"></span>
+                        </div>
+                        <h5>OnSite SEO</h5>
+                        <p>We analyse your website's structure, internal architecture & other key</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
+                    <div class="features">
+                        <div class="header mb-3">
+                            <span class="mai-business"></span>
+                        </div>
+                        <h5>OnSite SEO</h5>
+                        <p>We analyse your website's structure, internal architecture & other key</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
+                    <div class="features">
+                        <div class="header mb-3">
+                            <span class="mai-business"></span>
+                        </div>
+                        <h5>OnSite SEO</h5>
+                        <p>We analyse your website's structure, internal architecture & other key</p>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- .container -->
+</div> <!-- .page-section -->
