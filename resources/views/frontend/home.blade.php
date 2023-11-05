3215aa48250cd988971b7b3b7029d17cbd7cf7bd
Rangga8622 <ranggaekkle20020806@gmail.com>
2023-11-05 20:42:56 +0700

    update projek lading page home

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,15 +1,15 @@
 @extends('frontend.index')
 @section('content')
     <header>
-        <div class="container">
-            <div class="page-banner home-banner">
+        <div class="page-section bg-light">
+            <div class="container">
                 <div class="row align-items-center flex-wrap-reverse h-100">
                     <div class="col-md-6 py-5 wow fadeInLeft">
-                        <h1 class="mb-4">Let's Check and Optimize your website!</h1>
-                        <p class="text-lg text-grey mb-5">Ignite the most powerful growth engine you have ever built for your
-                            company</p>
-                        <a href="#" class="btn btn-primary btn-split">Watch Video <div class="fab"><span
-                                    class="mai-play"></span></div></a>
+                        <h1 class="mb-4">Selamat Datang di Website OrgEnroll</h1>
+                        <p class="text-lg text-grey mb-5">OrgEnroll adalah platform online yang memungkinkan mahasiswa untuk
+                            mendaftar ke berbagai organisasi yang ada di kampus</p>
+                        <a href="{{ url('about') }}" class="btn btn-primary btn-split">Mulai<div class="fab"><span
+                                    class="bi bi-caret-right-fill"></span></div></a>
                     </div>
                     <div class="col-md-6 py-5 wow zoomIn">
                         <div class="img-fluid text-center">
@@ -25,84 +25,59 @@
     <div class="page-section bg-light">
         <div class="container">
             <div class="text-center wow fadeInUp">
-                <div class="subhead">Our services</div>
-                <h2 class="title-section">How SEO Team Can Help</h2>
+
+                <h2 class="title-section">KELOMPOK 5 MENTOR 2</h2>
                 <div class="divider mx-auto"></div>
             </div>
 
             <div class="row">
                 <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
                     <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
+                        <div class="header mb-5">
+                            <img src="{{ asset('team/img/person_1.jpg') }}" alt="">
                         </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
+                        <h4>Ketua Kelompok</h4>
+                        <h5>Rangga Ekklesia Gabriel Anugrahnu</h5>
                     </div>
                 </div>
                 <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
                     <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
+                        <div class="header mb-5">
+                            <img src="{{ asset('team/img/person_1.jpg') }}" alt="">
                         </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
+                        <h4>Ketua Kelompok</h4>
+                        <h5>Rangga Ekklesia Gabriel Anugrahnu</h5>
                     </div>
                 </div>
                 <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
                     <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
+                        <div class="header mb-5">
+                            <img src="{{ asset('team/img/person_1.jpg') }}" alt="">
                         </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
+                        <h4>Ketua Kelompok</h4>
+                        <h5>Rangga Ekklesia Gabriel Anugrahnu</h5>
                     </div>
                 </div>
                 <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
                     <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
+                        <div class="header mb-5">
+                            <img src="{{ asset('team/img/person_1.jpg') }}" alt="">
                         </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
+                        <h4>Ketua Kelompok</h4>
+                        <h5>Rangga Ekklesia Gabriel Anugrahnu</h5>
                     </div>
                 </div>
                 <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
                     <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
+                        <div class="header mb-5">
+                            <img src="{{ asset('team/img/person_1.jpg') }}" alt="">
                         </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
+                        <h4>Ketua Kelompok</h4>
+                        <h5>Rangga Ekklesia Gabriel Anugrahnu</h5>
                     </div>
                 </div>
-                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
-                    <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
-                        </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
-                    <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
-                        </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
-                    <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
-                        </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
-                    </div>
-                </div>
+
+
             </div>
         </div> <!-- .container -->
     </div><!-- .page-section -->
