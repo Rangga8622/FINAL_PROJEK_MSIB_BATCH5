d08d67deb2e4ee4960ad2afdc1355784c3f19096
shafaslls <shafafebriani4@gmail.com>
2023-11-09 22:02:13 +0700

    ubah nama di organisasi

--- resources/views/backend/organisasi/index.blade.php
+++ resources/views/backend/organisasi/index.blade.php
@@ -8,7 +8,7 @@
 <div class="col-lg-12 grid-margin stretch-card">
     <div class="card">
         <div class="card-body">
-            <h4 class="card-title">Tabel Kategori</h4>
+            <h4 class="card-title">Tabel Organisasi</h4>
             <div class="table-responsive">
                 <table class="table table-striped">
                     <thead>
