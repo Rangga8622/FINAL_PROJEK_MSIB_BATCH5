52b3235636b87adfed2e0192f3a669ddae984b8d
Rangga8622 <ranggaekkle20020806@gmail.com>
2023-11-10 21:44:32 +0700

    update admin

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -31,7 +31,8 @@
                     <div class="col-md-12">
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb bg-light p-3 rounded">
-                                <li class="breadcrumb-item"><a href="#" class="text-primary">Home</a></li>
+                                <li class="breadcrumb-item"><a href="{{ url('/admin') }}"
+                                        class="text-primary">Dasboard</a></li>
                                 <li class="breadcrumb-item active" aria-current="page">Library</li>
                             </ol>
                         </nav>
