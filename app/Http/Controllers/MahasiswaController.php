c33e29e8fcf2eb1f9f00f13ff0aab5acd9ba310e
dmsandhika <dmsandhika87@students.unnes.ac.id>
2023-11-10 23:27:20 +0700

    fitur tambah dan upload  data di table  mahasiswa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -7,6 +7,8 @@ use App\Models\Mahasiswa;
 use App\Models\Jurusan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class MahasiswaController extends Controller
 {
@@ -25,17 +27,85 @@ class MahasiswaController extends Controller
     public function create()
     {
         //ambil master data kategori u/ dilooping di select option form
-        $ar_mahasiswa = Mahasiswa::all();
+        $ar_jurusan = Jurusan::all();
         $ar_gender = ['L', 'P'];
-        return view('backend.mahasiswa.form', compact('ar_mahasiswa','ar_gender'));
+        return view('backend.mahasiswa.form', compact('ar_jurusan','ar_gender'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate(
+            [
+                'nama' => 'required|max:45',
+                'idjurusan' => 'required|integer',
+                'semester' => 'required|max:45',
+                'gender' => 'required',
+                'nohp' => 'required|max:45',
+                'email' => 'required|max:45',
+                'cv' => 'required|mimes:pdf,doc|min:2|max:10000',
+                'foto' => 'nullable|image|mimes:jpg,jpeg,png,gif,svg|min:2|max:9000',
+
+            ],
+            [
+                'nama.required' => 'Nama Wajib Diisi',
+                'nama.max' => 'Nama Maksimal 45 Karakter',
+                'idjurusan.required' => 'Jurusan Wajib Diisi',
+                'idjurusan.integer' => 'Jurusan Wajib Dipilih',
+                'semester.required' => 'Semester Wajib Diisi',
+                'semester.max' => 'Semester Wajib Diisi',
+                'gender.required' => 'Gender Wajib Dipilih',
+                'nohp.required' => 'No HP Wajib Diisi',
+                'nohp.max' => 'No HP Maksimal 45 Karakter',
+                'email.required' => 'Email Wajib Diisi',
+                'email.max' => 'Email Maksimal 45 Karakter',
+                'cv.required' => 'CV Wajib Diupload',
+                'cv.mimes' => 'Format CV Wajib PDF/DOC',
+                'cv.min' => 'Ukuran Minimum CV 2 KB',
+                'cv.max' => 'Ukuran Maximum CV 10 MB',
+                'foto.image' => 'Foto Harus Berupa Gambar',
+                'foto.mimes' => 'Foto Harus Format JPG, JPEG, PNG, GIF, SVG',
+            ]
+
+            );
+            if (!empty($request->cv)){
+                $fileNamee = 'mhscv_'.date("Ymd_h-i-s").'.'.$request->cv->extension();
+                $request->cv->move(public_path('backend/mhs/cv'),$fileNamee);
+            }
+            else{
+                $fileNamee = '';
+            }
+            if(!empty($request->foto)){
+                $fileName = 'mhs_'.date("Ymd_h-i-s").'.'.$request->foto->extension();
+                $request->foto->move(public_path('backend/mhs/foto'),$fileName);
+            }
+            else{
+                $fileName = '';
+            }
+
+            try{
+                DB::table('mahasiswa')->insert(
+                    [
+                        'nama'=>$request->nama,
+                        'idjurusan'=>$request->idjurusan,
+                        'semester'=>$request->semester,
+                        'gender'=>$request->gender,
+                        'nohp'=>$request->nohp,
+                        'email'=>$request->email,
+                        'cv'=>$fileNamee,
+                        'foto'=>$fileName,
+                    ]
+                    );
+                    return redirect()->route('mahasiswa.index')
+                            ->with('success','Data Asset Baru Berhasil Disimpan');
+            }
+            catch (\Exception $e){
+                //return redirect()->back()
+                return redirect()->route('mahasiswa.index')
+                    ->with('error', 'Terjadi Kesalahan Saat Input Data!');
+            }
     }
 
     /**
