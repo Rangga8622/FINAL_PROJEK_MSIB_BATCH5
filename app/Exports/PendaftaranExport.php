168163ae589efc3a636455ecd6a06eecc0f68784
Rangga8622 <ranggaekkle20020806@gmail.com>
2023-11-24 07:43:13 +0700

    perbaikan posisi judul pada tabel ketika di eksport ke excel

--- app/Exports/PendaftaranExport.php
+++ app/Exports/PendaftaranExport.php
@@ -10,22 +10,24 @@ use Illuminate\Support\Facades\DB;
 class PendaftaranExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         //return Pendaftaran::all();
         $ar_pendaftaran = DB::table('pendaftaran as p')
-        ->join('mahasiswa as m', 'p.idmahasiswa', '=', 'm.id')
-        ->join('organisasi as o', 'm.idorganisasi', '=', 'o.id')
-        ->join('jurusan as j', 'm.idjurusan', '=', 'j.id')
-        ->select('m.nama as nama_mahasiswa', 'j.nama', 'o.nama as nama_organisasi','m.tanggal_pendaftaran as tgl', 'p.status_pendaftaran', 'p.keterangan')
-        ->get();
-            return $ar_pendaftaran;
+            ->join('mahasiswa as m', 'p.idmahasiswa', '=', 'm.id')
+            ->join('organisasi as o', 'm.idorganisasi', '=', 'o.id')
+            ->join('jurusan as j', 'm.idjurusan', '=', 'j.id')
+            ->select('m.nama as nama_mahasiswa', 'j.nama', 'o.nama as nama_organisasi', 'm.tanggal_pendaftaran as tgl', 'p.status_pendaftaran', 'p.keterangan')
+            ->get();
+        return $ar_pendaftaran;
     }
     public function headings(): array
     {
-        return ["Nama", "Organisasi","Jurusan", "Tanggal Pendaftaran", "Status Pendaftaran",
-                "Keterangan"];
+        return [
+            "Nama", "Jurusan", "Organisasi", "Tanggal Pendaftaran", "Status Pendaftaran",
+            "Keterangan"
+        ];
     }
 }
