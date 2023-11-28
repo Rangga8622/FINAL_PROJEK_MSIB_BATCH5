c3f6455c5d775697e2890c3624d5872db3721087
Rangga8622 <ranggaekkle20020806@gmail.com>
2023-11-28 08:16:35 +0700

    perbaiki tampilan ui di login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,106 +1,116 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>ORGENROLL</title>
-  <!-- plugins:css -->
-  <link rel="stylesheet" href="{{ asset('backend/vendors/ti-icons/css/themify-icons.css') }}">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>ORGENROLL</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('backend/vendors/ti-icons/css/themify-icons.css') }}">
     <link rel="stylesheet" href="{{ asset('backend/vendors/base/vendor.bundle.base.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-  <!-- endinject -->
-  <!-- plugin css for this page -->
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
-  <link rel="stylesheet" href="{{ asset('backend/css/style.css') }}">
-  <!-- endinject -->
-  <link rel="shortcut icon" href="{{ asset('backend/img/logo.jpg') }}"  />
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('backend/css/style.css') }}">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="{{ asset('backend/img/logo.jpg') }}" />
 </head>
 
 <body>
-  <div class="container-scroller">
-    <div class="container-fluid page-body-wrapper full-page-wrapper">
-      <div class="content-wrapper d-flex align-items-center auth px-0">
-        <div class="row w-100 mx-0">
-          <div class="col-lg-4 mx-auto">
-            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
-              <div class="brand-logo">
-                  <img src="../../img/logo1.png" alt="">
-              </div>
-              
-              <h5 align="center">WELCOME TO ORGENROLL</h4>
-              <form class="pt-3" method="POST" action="{{ route('login') }}">
-              @csrf
-                <div class="form-group">
-                  <label for="exampleInputEmail">Email</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend bg-transparent">
-                      <span class="input-group-text bg-transparent border-right-0">
-                        <i class="ti-user text-primary"></i>
-                      </span>
-                    </div>
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email" autofocus>
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputPassword">Password</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend bg-transparent">
-                      <span class="input-group-text bg-transparent border-right-0">
-                        <i class="ti-lock text-primary"></i>
-                      </span>
-                    </div>
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                  </div>
-                </div>
+    <div class="container-scroller">
+        <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="content-wrapper d-flex align-items-center auth px-0">
+                <div class="row w-100 mx-0">
+                    <div class="col-lg-4 mx-auto">
+                        <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+                            <div class="brand-logo">
+                                <img src="../../img/logo1.png" alt="">
+                            </div>
 
-                <div class="mt-3">
-                    <button type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">
-                    {{ __('Login') }}
-                    </button>
-                </div>
-</br>
-                <div class="my-2 d-flex justify-content-between align-items-center">
+                            <h5 align="center">WELCOME TO ORGENROLL</h4>
+                                <form class="pt-3" method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail">Email</label>
+                                        <div class="input-group">
+                                            <div class="input-group-prepend bg-transparent">
+                                                <span class="input-group-text bg-transparent border-right-0">
+                                                    <i class="ti-user text-primary"></i>
+                                                </span>
+                                            </div>
+                                            <input id="email" type="email"
+                                                class="form-control @error('email') is-invalid @enderror" name="email"
+                                                value="{{ old('email') }}" required
+                                                oninvalid="this.setCustomValidity('Form email tidak boleh kosong')"
+                                                oninput="setCustomValidity('')" autocomplete="email" placeholder="Email"
+                                                autofocus>
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword">Password</label>
+                                        <div class="input-group">
+                                            <div class="input-group-prepend bg-transparent">
+                                                <span class="input-group-text bg-transparent border-right-0">
+                                                    <i class="ti-lock text-primary"></i>
+                                                </span>
+                                            </div>
+                                            <input id="password" type="password"
+                                                class="form-control @error('password') is-invalid @enderror"
+                                                name="password" required oninvalid="this.setCustomValidity('Form Password tidak boleh kosong')"
+                                                oninput="setCustomValidity('')" autocomplete="current-password"
+                                                placeholder="Password">
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="mt-3">
+                                        <button type="submit"
+                                            class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">
+                                            {{ __('Login') }}
+                                        </button>
+                                    </div>
+                                    </br>
+                                    {{-- <div class="my-2 d-flex justify-content-between align-items-center">
                   <a href="#" class="auth-link text-black">Forgot password?</a>
+                </div> --}}
+                                    </br>
+                                    <div class="text-center mt-4 font-weight-light">
+                                        Apakah anda tidak memiliki akun ? <a href="{{ url('/register') }}"
+                                            class="text-primary">
+                                            {{ __('Buat Akun') }}
+                                        </a>
+                                    </div>
+                                </form>
+                        </div>
+                    </div>
                 </div>
-</br>
-                <div class="text-center mt-4 font-weight-light">
-                  Don't have an account? <a href="{{ url('/register')}}" class="text-primary">                                    
-                    {{ __('Register User') }}
-                  </a>
-                </div>
-              </form>
             </div>
-          </div>
+            <!-- content-wrapper ends -->
         </div>
-      </div>
-      <!-- content-wrapper ends -->
+        <!-- page-body-wrapper ends -->
     </div>
-    <!-- page-body-wrapper ends -->
-  </div>
-  <!-- container-scroller -->
-  <!-- plugins:js -->
-  <script src="{{ asset('backend/vendors/base/vendor.bundle.base.js') }}"></script>
-  <!-- endinject -->
-  <!-- inject:js -->
-  <script src="{{ asset('backend/js/off-canvas.js') }}') }}"></script>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="{{ asset('backend/vendors/base/vendor.bundle.base.js') }}"></script>
+    <!-- endinject -->
+    <!-- inject:js -->
+    <script src="{{ asset('backend/js/off-canvas.js') }}') }}"></script>
     <script src="{{ asset('backend/js/hoverable-collapse.js') }}"></script>
     <script src="{{ asset('backend/js/template.js') }}"></script>
     <script src="{{ asset('backend/js/todolist.js') }}"></script>
-  <!-- endinject -->
+    <!-- endinject -->
 </body>
 
 </html>
