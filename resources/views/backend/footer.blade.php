73512ed0704238c291400eda62cc791d68809b05
Rangga8622 <ranggaekkle20020806@gmail.com>
2023-11-29 08:00:26 +0700

    perbaiki tampilan footer

--- resources/views/backend/footer.blade.php
+++ resources/views/backend/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
           <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a
                 href="https://www.bootstrapdash.com/" target="_blank">FIVE GROUP PROJECT </a>2023</span>
             <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">KELOMPOK 5 MENTORING 2 <a
                 href="https://www.bootstrapdash.com/" target="_blank"> ORGENROLL </a> PROJECT</span>
